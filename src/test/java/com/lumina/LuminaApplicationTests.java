96fdd74558c0b03cd2f872cbfb743270f1865cb9
jojo <1911572936@qq.com>
2026-01-10 16:02:23 +0800

    Initial commit: Java Spring Boot project setup for Lumina
    - Set up Maven project structure with pom.xml
    - Configured Spring Boot 3.2.0 with Java 17
    - Added MyBatis Plus integration for MySQL
    - Created basic package structure
    - Added .gitignore for Java/Maven projects

---
+++ src/test/java/com/lumina/LuminaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lumina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuminaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
