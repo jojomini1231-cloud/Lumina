03489628dc12a35949af14bffef22094d7c45377
jojo <1911572936@qq.com>
2026-01-14 13:16:51 +0800

    refactor: 优化RelayController逻辑

--- src/main/java/com/lumina/controller/RelayController.java
+++ src/main/java/com/lumina/controller/RelayController.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.lumina.service.RelayService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,8 +26,22 @@ public class RelayController {
      * @return
      */
     @PostMapping("/v1/messages")
-    public Flux<String> createMessage(@RequestBody ObjectNode params,Boolean beta) {
-        Flux<String> anthropicMessages = relayService.relay("anthropic_messages", params, beta);
+    public Object createMessage(@RequestBody ObjectNode params, Boolean beta) {
+        Object anthropicMessages = relayService.relay("anthropic_messages", params, beta);
+        return anthropicMessages;
+    }
+
+    /**
+     * 中转 OpenAI 格式（Chat Completions）
+     * @param params
+     * @return
+     */
+    @PostMapping(
+            value = "/v1/chat/completions",
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Object createChatCompletions(@RequestBody ObjectNode params,Boolean beta) {
+        Object anthropicMessages = relayService.relay("openai_chat_completions", params, beta);
         return anthropicMessages;
     }
 
